49d05babfcc5180b65361ab2e87953ffdff7c691
Rydedispatch <emmanuel.o@mtsryde.com>
2025-11-14 18:31:13 -0600

    Revert to standard Prisma client - edge client not compatible

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,13 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined
+  prisma: PrismaClient | undefined
 }
 
-const createPrismaClient = () => {
-  return new PrismaClient({
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  }).$extends(withAccelerate())
-}
-
-export const prisma = globalForPrisma.prisma ?? createPrismaClient()
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
